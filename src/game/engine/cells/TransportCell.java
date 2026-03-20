406efac626c21d8759ac6d20f2fc8000b07c90d5
Eyad-Amgad-10 <eyad.sharaf@student.giu-uni.de>
2026-03-20 19:24:54 +0200

    Update TransportCell.java

--- src/game/engine/cells/TransportCell.java
+++ src/game/engine/cells/TransportCell.java
@@ -1,5 +1,16 @@
 package game.engine.cells;
 
-public abstract class TransportCell {
+public abstract class TransportCell extends Cell{
+	private int effect;
+	
+	public TransportCell(String name, int effect) {
+		super(name);
+		this.effect = effect;
+	}
+
+	public int getEffect() {
+		return effect;
+	}
+	
 
 }
