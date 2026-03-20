2480a15b3c1255d6e8756aa28bcbae71414b0cfe
OmarKhaled1212 <omarkhaled1212@outlook.com>
2026-03-20 19:04:13 +0200

    Cards Completed
    ....

--- src/game/engine/cards/EnergyStealCard.java
+++ src/game/engine/cards/EnergyStealCard.java
@@ -1,5 +1,13 @@
 package game.engine.cards;
 
-public class EnergyStealCard {
+public class EnergyStealCard extends Card{
+private int energy;
 
+public EnergyStealCard(String name, String description, int rarity, boolean lucky, int energy) {
+	super(name,description,rarity,true);
+	this.energy=energy;
+}
+public int getEnergy() {
+	return energy;
+}
 }
--- src/game/engine/cards/SwapperCard.java
+++ src/game/engine/cards/SwapperCard.java
@@ -1,5 +1,8 @@
 package game.engine.cards;
 
-public class SwapperCard {
+public class SwapperCard extends Card{
 
+	public SwapperCard(String name, String description, int rarity, boolean lucky) {
+		super(name,description,rarity,true);
+	}
 }
