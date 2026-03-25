921deb665f2d640267c2b1b19dd234ff70f49e2a
Ahmed Elmewafy <ahmedalmewafy@gmail.com>
2026-03-25 14:08:40 +0200

    Game class complete.

--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -1,5 +1,8 @@
 package game.engine;
-import java.util.*; import game.engine.monsters.*; import game.engine.dataloader.*; import game.engine.cards.*;
+import java.util.*; 
+import game.engine.monsters.*; 
+import game.engine.dataloader.*; 
+import java.io.*;
 public class Game {
 private Board board;
 private ArrayList<Monster> allMonsters= new ArrayList<>();
@@ -7,11 +10,16 @@ private Monster player;
 private Monster opponent;
 private Monster current;
 
-public Game(Role playerRole) throws IOException{
-	try (ArrayList<Card> BoardCards=DataLoader.readCards()){
-	ArrayList<Card>CellCards=Board.getOriginalCards();
-	CellCards=BoardCards;}
+public Game (Role playerRole) throws IOException
+{
+	board = new Board (DataLoader.readCards());
+	allMonsters = DataLoader.readMonsters();
+	player = selectRandomMonsterByRole(playerRole);
+	Role oppositeRole = (playerRole == Role.SCARER)? Role.LAUGHER: Role.SCARER ;
+	opponent = selectRandomMonsterByRole(oppositeRole); 
+	current = player;
 }
+
 
 private Monster selectRandomMonsterByRole(Role role) {
 	return allMonsters.get((int)(Math.random()*6)+1);
@@ -19,7 +27,7 @@ private Monster selectRandomMonsterByRole(Role role) {
 public Board getBoard() {
 	return board;
 }
-public ArrayList getAllMonsters() {
+public ArrayList <Monster> getAllMonsters() {
 	return allMonsters;
 }
 public Monster getPlayer() {
