5d05eb6e38cf4311d15a2cb3d63cd49e4a788e2a
Ahmed Elmewafy <ahmedalmewafy@gmail.com>
2026-03-25 18:18:08 +0200

    More fixes for milestone 1

--- src/game/engine/cells/ContaminationSock.java
+++ src/game/engine/cells/ContaminationSock.java
@@ -3,8 +3,8 @@ import game.engine.interfaces.*;
 public class ContaminationSock extends TransportCell implements CanisterModifier{
 	
 	public ContaminationSock(String name, int effect)  {
-		int tempEffect = (effect < 0)? effect: -effect;
-		super(name, tempEffect);
+		super(name, (effect < 0)? effect: -effect);
 	}
 
 }
+	
